5b5994e4ccef341c5253d99925ac8ce32138278c
femto-code <59444046+femto-code@users.noreply.github.com>
2021-02-26 00:30:07 +0100

    Fixed navbar design layout and new icon
    replaced RPi official png icon with svg / improved navbar markdown

--- index.php
+++ index.php
@@ -104,8 +104,10 @@ if($auth){
 <div class="preload-screen"></div>
 
 <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark shadow-sm">
-  <img class="rounded float-left" src="img/rpi_logo.png" alt="Responsive image" height="30px" width="auto">&nbsp;<!--50px height navbar default-->
-  <a class="navbar-brand" href="#">Raspberry Pi Dashboard</a>
+  <a class="navbar-brand" href="./">
+    <img src="img/official_logo.svg" width="30" height="30" class="d-inline-block align-top" alt="RPi Logo">
+    Raspberry Pi Dashboard
+  </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
