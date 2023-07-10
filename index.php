b0408f4b3ce84360f6abe2665d675e70cf8c2204
femto-code <59444046+femto-code@users.noreply.github.com>
2023-07-11 01:18:55 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 session_start();
 error_reporting (E_ALL);
 ini_set ('display_errors', 'On');
+
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
 $auth=(isset($_SESSION["rpidbauth"])) ? true : false;
 
 require "backend/Config.php";
