f1dae7d24a837301f17216bd8db88b58b22892e8
femto-code <59444046+femto-code@users.noreply.github.com>
2023-10-12 00:31:26 +0200

    :arrow_right: Move CSS to external file

--- index.php
+++ index.php
@@ -52,128 +52,6 @@ $string = trim(preg_replace('/\s\s+/', '', shell_exec("hostname")));
 
 <title><?php system("hostname");?> - Loading...</title>
 
-<style>
-/* rubik-300 - latin */
-@font-face {
-  font-family: 'Rubik';
-  font-style: normal;
-  font-weight: 300;
-  src: url('fonts/rubik-v12-latin-300.eot'); /* IE9 Compat Modes */
-  src: local(''),
-    url('fonts/rubik-v12-latin-300.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
-    url('fonts/rubik-v12-latin-300.woff2') format('woff2'), /* Super Modern Browsers */
-    url('fonts/rubik-v12-latin-300.woff') format('woff'), /* Modern Browsers */
-    url('fonts/rubik-v12-latin-300.ttf') format('truetype'), /* Safari, Android, iOS */
-    url('fonts/rubik-v12-latin-300.svg#Rubik') format('svg'); /* Legacy iOS */
-}
-body, .mdtoast{
-  font-family: 'Rubik', sans-serif;
-}
-.hidden{
-  display: none;
-}
-@media screen and (max-width: 530px) {
-  #notf {
-    display: block;
-  }
-  #dot{
-    display:none;
-  }
-}
-.preload-screen {
-  position: fixed;
-  left: 0px;
-  top: 0px;
-  width: 100%;
-  height: 100%;
-  z-index: 9999;
-  background: url(img/load.gif) center no-repeat #fff;
-}
-.doughnut-chart-container {
-  height: 360px;
-  width: 360px;
-  float: left;
-}
-</style>
-<style>
-.bd-placeholder-img {
-  font-size: 1.125rem;
-  text-anchor: middle;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  user-select: none;
-}
-
-@media (min-width: 768px) {
-  .bd-placeholder-img-lg {
-    font-size: 3.5rem;
-  }
-}
-
-.b-example-divider {
-  width: 100%;
-  height: 3rem;
-  background-color: rgba(0, 0, 0, .1);
-  border: solid rgba(0, 0, 0, .15);
-  border-width: 1px 0;
-  box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
-}
-
-.b-example-vr {
-  flex-shrink: 0;
-  width: 1.5rem;
-  height: 100vh;
-}
-
-.bi {
-  /* vertical-align: -.125em; */
-  fill: currentColor;
-}
-
-.nav-scroller {
-  position: relative;
-  z-index: 2;
-  height: 2.75rem;
-  overflow-y: hidden;
-}
-
-.nav-scroller .nav {
-  display: flex;
-  flex-wrap: nowrap;
-  padding-bottom: 1rem;
-  margin-top: -1px;
-  overflow-x: auto;
-  text-align: center;
-  white-space: nowrap;
-  -webkit-overflow-scrolling: touch;
-}
-
-.btn-bd-primary {
-  --bd-violet-bg: #712cf9;
-  --bd-violet-rgb: 112.520718, 44.062154, 249.437846;
-
-  --bs-btn-font-weight: 600;
-  --bs-btn-color: var(--bs-white);
-  --bs-btn-bg: var(--bd-violet-bg);
-  --bs-btn-border-color: var(--bd-violet-bg);
-  --bs-btn-hover-color: var(--bs-white);
-  --bs-btn-hover-bg: #6528e0;
-  --bs-btn-hover-border-color: #6528e0;
-  --bs-btn-focus-shadow-rgb: var(--bd-violet-rgb);
-  --bs-btn-active-color: var(--bs-btn-hover-color);
-  --bs-btn-active-bg: #5a23c8;
-  --bs-btn-active-border-color: #5a23c8;
-}
-
-.bd-mode-toggle {
-  z-index: 1500;
-}
-
-.bd-mode-toggle .dropdown-menu .active .bi {
-  display: block !important;
-}
-</style>
-
 <?php
 if($auth){
   $upt=new DateTime(shell_exec('uptime -s'));
@@ -206,7 +84,7 @@ if($auth){
 
 </head>
 <body onload="preload()">
-<noscript style="z-index: 99999!important; position: absolute; top: 0; width: 98%; padding: 3%;"><div class="alert alert-danger" role="alert">Raspberry Pi Dashboard Web Application <b>requires</b> JavaScript to be enabled in order to work properly - enable it to continue!</div></noscript>
+<noscript style="z-index: 99999!important; position: absolute; top: 0; width: 98%; padding: 3%;"><div class="alert alert-danger" role="alert">Raspberry Pi Dashboard as a web application <b>requires</b> JavaScript to be enabled in order to work properly!</div></noscript>
 
 <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
   <symbol id="check2" viewBox="0 0 16 16">
